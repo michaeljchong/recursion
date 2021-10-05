1df0156ff9542a2b83c06eb357842aa4f887911c
Michael Chong <mikec0920@gmail.com>
2021-10-05 12:44:03 -0700

    Implemented recursive fibonacci

--- recursion.rb
+++ recursion.rb
@@ -1,7 +1,7 @@
 def fibs(num, sequence = [])
-  num.times do |i|
+  (0..num - 1).each do |i|
     if sequence.length < 2
-      sequence << 1
+      sequence << i
     else
       sequence << sequence[-2] + sequence[-1]
     end
@@ -9,4 +9,15 @@ def fibs(num, sequence = [])
   sequence
 end
 
-p fibs(3)
+def fibs_rec(num, fib = [])
+  if num <= 2
+    (0...num).each { |i| fib << i }
+    return fib
+  end
+
+  fib = fibs_rec(num - 1)
+  fib << fib[-2] + fib[-1]
+end
+
+p fibs(2)
+p fibs_rec(3)
